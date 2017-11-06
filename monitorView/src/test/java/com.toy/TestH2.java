979b6146258202b3382ee56500b6f12d84a0620f
ljx <491327260@qq.com>
2017-11-06 09:26:10 +0800

    v1

--- monitorView/src/test/java/com.toy/TestH2.java
+++ monitorView/src/test/java/com.toy/TestH2.java
@@ -10,8 +10,22 @@ public class TestH2 {
     public void startH2() throws SQLException, InterruptedException {
         H2DB h2 = new H2DB();
         h2.start();
-        while (true){
-            Thread.sleep(1);
-        }
+    }
+
+    public static void main(String[] arge) throws SQLException {
+        H2DB h2 = new H2DB();
+        h2.start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (true) {
+                        Thread.sleep(100000);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
